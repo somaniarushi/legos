6a7504fb76cde8eabd6d61c96048b992ae4ec18d
Arushi Somani <arushisomani@berkeley.edu>
2022-01-16 02:15:14 +0530

    adding api endpoints

--- server.py
+++ server.py
@@ -14,7 +14,30 @@ blockchain = Blockchain()
 
 @app.route('/mine', methods=['GET'])
 def mine():
-    return 'Mining a new block'
+    last_block = blockchain.last_block
+    last_proof = last_block['proof']
+    proof = blockchain.proof_of_work(last_proof)
+
+    # sender = 0 means that this node has mined a new coin
+    blockchain.add_transaction(
+        sender="0",
+        recipient=node_identifier,
+        amount=1,
+    )
+
+    previous_hash = blockchain.hash(last_block)
+    block = blockchain.add_block(proof, previous_hash)
+
+    response = {
+        'message': 'New block forged.',
+        'index': block['index'],
+        'transactions': block['transactions'],
+        'proof': block['proof'],
+        'previous_hash': block['previous_hash']
+    }
+
+    return jsonify(response), 200
+
 
 @app.route('/transactions/new', methods=['POST'])
 def new_transaction():
