1e44fcee5eca25438b139ce18f5bd359faa934fc
Arushi Somani <arushisomani@berkeley.edu>
2022-01-16 01:19:54 +0530

    adding proof of work

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-# Legos: Python Implementation of a Blockchain
+# Legos: Implementing a Blockchain
+
+Inspired by Daniel Van Flymen's excellent article on the inner workings of the blockchain.
 
 ## Requirements
 - Python 3.6+
@@ -11,3 +13,11 @@ Each blockchain has a list of block and a list of transactions made over that ch
 Each Block has an index, a timestamp (in Unix time), a list of transactions, a proof, and the hash of the previous Block.
 
 Each transaction has the id of the sender and the recipient, as well as the amount sent over the chain.
+
+## Proof of Work Implementation
+We need to introduce some sort of _number_ that is the confirmation of a new block being created onto the blockchain. The number must be computationally **difficult to find and easy to verify**.
+
+We'll be using a [HashCash](https://en.wikipedia.org/wiki/Hashcash?ref=hackernoon.com) implementation.
+
+Our next proof depends on the previous proof. It is generated as follows:
+> When a number p when hashed with the previous block's solution has a hash with 4 leading 0s, this is our next proof.
--- blockchain.py
+++ blockchain.py
@@ -1,3 +1,7 @@
+import hashlib
+import json
+from time import time
+
 class Blockchain(object):
     '''
     Represents a block chain in our system.
@@ -5,32 +9,83 @@ class Blockchain(object):
     def __init__(self):
         self.chain = []
         self.transactions = []
+        self.__genesis()
 
-    def add_block(self):
+    def __genesis(self):
         '''
-        Creates a new block and adds it to the chain
+        Starts the chain with a block.
         '''
-        pass
+        self.add_block(previous_hash=1, proof=100)
 
-    def add_transaction(self):
+    def add_block(self, proof, previous_hash=None):
+        '''
+        Creates a new block and adds it to the chain. Returns the new block.
+        Uses the proof value given by the proof of work algorithm
+        Optionally accepts the hash of the previous block. #TODO: why optional?
+        '''
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'transactions': self.transactions,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1])
+        }
+
+        # All current transactions have been added to the block, null out
+        self.transactions = []
+
+        self.chain.append(block)
+        return block
+
+    def add_transaction(self, sender, recipient, amount):
         '''
         Creates a new transaction and adds it to the list of
         transactions.
+        Returns the index of the block that holds this transaction.
+        This block is the next one to be mined.
         '''
-        pass
+        self.transactions.append({
+            'sender': sender,
+            'recipient': recipient,
+            'amount': amount
+        })
+
+        return self.last_block['index'] + 1
+
+    def proof_of_work(self, last_proof):
+        '''
+        Given last proof, return a number that when hashed with
+        the previous block's solution has a hash with 4 leading 0s
+        '''
+        proof = 0
+        while not self.valid_proof(last_proof, proof):
+            proof += 1
+        return proof
+
+    @staticmethod
+    def valid_proof(last_proof, proof):
+        '''
+        Validates a new proof based on given proof of work algorithm
+        and returns True or False respectively.
+        '''
+        guess = f'{last_proof}{proof}'.encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+        return guess_hash[:4] == '0000'
 
     @staticmethod
     def hash(block):
         '''
-        Accepts a block on the chain and
-        returns its hascodes
+        Accepts a block on the chain and returns its hascodes.
+        Hash algo: SHA256
         '''
-        pass
+        # Order the dictionary, then convert to JSON string
+        block_string = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
 
     @property
     def last_block(self):
         '''
         Returns the current last block on the chain
         '''
-        pass
+        return self.chain[-1]
 
